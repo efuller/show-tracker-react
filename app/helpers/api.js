46364d9353ddd72e5193007e22b9c45ea1b9711e
Eric Fuller <info@glassmoonmedia.com>
2019-02-05 20:21:06 -0500

    update api.js

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,20 +1,15 @@
 import axios from 'axios';
 import config from '../config/config';
 
-const _API_KEY = config.API_KEY;
-const _URL = `https://api.themoviedb.org/3`;
+const API_KEY = config.API_KEY;
+const URL = 'https://api.themoviedb.org/3';
 
 export function getShows(show) {
-	return axios(`${_URL}/search/tv?api_key=${_API_KEY}&query=${show}`)
-		.then(function(showData) {
-			return showData;
-		});
+	return axios(`${URL}/search/tv?api_key=${API_KEY}&query=${show}`)
+		.then(showData => showData);
 }
 
 export function getShowDetails(showID) {
-	return axios(`${_URL}/tv/${showID}?api_key=${_API_KEY}`)
-		.then(function(showDetails) {
-			console.log(showDetails);
-		})
+	return axios(`${URL}/tv/${showID}?api_key=${API_KEY}`)
+		.then(showDetails => showDetails);
 }
-
